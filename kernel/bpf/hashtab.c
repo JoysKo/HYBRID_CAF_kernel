de676980f6097cd7b278bed7b74a7e9682222f89
tom.leiming@gmail.com <tom.leiming@gmail.com>
2015-12-29 22:40:27 +0800

    bpf: hash: use per-bucket spinlock
    Both htab_map_update_elem() and htab_map_delete_elem() can be
    called from eBPF program, and they may be in kernel hot path,
    so it isn't efficient to use a per-hashtable lock in this two
    helpers.
    
    The per-hashtable spinlock is used for protecting bucket's
    hlist, and per-bucket lock is just enough. This patch converts
    the per-hashtable lock into per-bucket spinlock, so that
    contention can be decreased a lot.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/bpf/hashtab.c
+++ kernel/bpf/hashtab.c
@@ -23,10 +23,14 @@ struct bucket {
 	raw_spinlock_t lock;
 };
 
+struct bucket {
+	struct hlist_head head;
+	raw_spinlock_t lock;
+};
+
 struct bpf_htab {
 	struct bpf_map map;
-	struct hlist_head *buckets;
-	raw_spinlock_t lock;
+	struct bucket *buckets;
 	atomic_t count;	/* number of elements in this hashtable */
 	u32 n_buckets;	/* number of hash buckets */
 	u32 elem_size;	/* size of each element in bytes */
@@ -212,32 +216,31 @@ static struct bpf_map *htab_map_alloc(union bpf_attr *attr)
 	    htab->n_buckets > U32_MAX / sizeof(struct bucket))
 		goto free_htab;
 
-	if ((u64) htab->n_buckets * sizeof(struct hlist_head) +
+	if ((u64) htab->n_buckets * sizeof(struct bucket) +
 	    (u64) htab->elem_size * htab->map.max_entries >=
 	    U32_MAX - PAGE_SIZE)
 		/* make sure page count doesn't overflow */
 		goto free_htab;
 
-	htab->map.pages = round_up(htab->n_buckets * sizeof(struct hlist_head) +
+	htab->map.pages = round_up(htab->n_buckets * sizeof(struct bucket) +
 				   htab->elem_size * htab->map.max_entries,
 				   PAGE_SIZE) >> PAGE_SHIFT;
 
 	err = -ENOMEM;
-	htab->buckets = kmalloc_array(htab->n_buckets, sizeof(struct hlist_head),
+	htab->buckets = kmalloc_array(htab->n_buckets, sizeof(struct bucket),
 				      GFP_USER | __GFP_NOWARN);
 
+	if (!htab->buckets) {
+		htab->buckets = vmalloc(htab->n_buckets * sizeof(struct bucket));
+		if (!htab->buckets)
+			goto free_htab;
+	}
+
 	for (i = 0; i < htab->n_buckets; i++) {
-		INIT_HLIST_NULLS_HEAD(&htab->buckets[i].head, i);
+		INIT_HLIST_HEAD(&htab->buckets[i].head);
 		raw_spin_lock_init(&htab->buckets[i].lock);
 	}
 
-	if (!percpu) {
-		err = alloc_extra_elems(htab);
-		if (err)
-			goto free_buckets;
-	}
-
-	raw_spin_lock_init(&htab->lock);
 	atomic_set(&htab->count, 0);
 
 	return &htab->map;
@@ -261,13 +264,12 @@ static inline struct bucket *__select_bucket(struct bpf_htab *htab, u32 hash)
 	return &htab->buckets[hash & (htab->n_buckets - 1)];
 }
 
-static inline struct hlist_nulls_head *select_bucket(struct bpf_htab *htab, u32 hash)
+static inline struct hlist_head *select_bucket(struct bpf_htab *htab, u32 hash)
 {
 	return &__select_bucket(htab, hash)->head;
 }
 
-/* this lookup function can only be called with bucket lock taken */
-static struct htab_elem *lookup_elem_raw(struct hlist_nulls_head *head, u32 hash,
+static struct htab_elem *lookup_elem_raw(struct hlist_head *head, u32 hash,
 					 void *key, u32 key_size)
 {
 	struct hlist_nulls_node *n;
@@ -535,8 +537,9 @@ static int htab_map_update_elem(struct bpf_map *map, void *key, void *value,
 				u64 map_flags)
 {
 	struct bpf_htab *htab = container_of(map, struct bpf_htab, map);
-	struct htab_elem *l_new = NULL, *l_old;
-	struct hlist_nulls_head *head;
+	struct htab_elem *l_new, *l_old;
+	struct hlist_head *head;
+	struct bucket *b;
 	unsigned long flags;
 	struct bucket *b;
 	u32 key_size, hash;
@@ -558,7 +561,8 @@ static int htab_map_update_elem(struct bpf_map *map, void *key, void *value,
 	hash = htab_map_hash(key, key_size);
 
 	l_new->hash = htab_map_hash(l_new->key, key_size);
-	head = select_bucket(htab, l_new->hash);
+	b = __select_bucket(htab, l_new->hash);
+	head = &b->head;
 
 	/* bpf_map_update_elem() can be called in_irq() */
 	raw_spin_lock_irqsave(&b->lock, flags);
@@ -641,9 +645,12 @@ static int __htab_percpu_map_update_elem(struct bpf_map *map, void *key,
 	} else {
 		atomic_inc(&htab->count);
 	}
-	ret = 0;
+	raw_spin_unlock_irqrestore(&b->lock, flags);
+
+	return 0;
 err:
 	raw_spin_unlock_irqrestore(&b->lock, flags);
+	kfree(l_new);
 	return ret;
 }
 
@@ -657,7 +664,7 @@ static int htab_percpu_map_update_elem(struct bpf_map *map, void *key,
 static int htab_map_delete_elem(struct bpf_map *map, void *key)
 {
 	struct bpf_htab *htab = container_of(map, struct bpf_htab, map);
-	struct hlist_nulls_head *head;
+	struct hlist_head *head;
 	struct bucket *b;
 	struct htab_elem *l;
 	unsigned long flags;
@@ -669,9 +676,10 @@ static int htab_map_delete_elem(struct bpf_map *map, void *key)
 	key_size = map->key_size;
 
 	hash = htab_map_hash(key, key_size);
-	head = select_bucket(htab, hash);
+	b = __select_bucket(htab, hash);
+	head = &b->head;
 
-	raw_spin_lock_irqsave(&htab->lock, flags);
+	raw_spin_lock_irqsave(&b->lock, flags);
 
 	l = lookup_elem_raw(head, hash, key, key_size);
 
