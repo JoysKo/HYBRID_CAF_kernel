dad8333f27c7a0e4b7e3ac3a42b6c1ae80f0e31e
Alexei Starovoitov <ast@fb.com>
2016-03-09 20:02:33 -0800

    bpf: avoid copying junk bytes in bpf_get_current_comm()
    Lots of places in the kernel use memcpy(buf, comm, TASK_COMM_LEN); but
    the result is typically passed to print("%s", buf) and extra bytes
    after zero don't cause any harm.
    In bpf the result of bpf_get_current_comm() is used as the part of
    map key and was causing spurious hash map mismatches.
    Use strlcpy() to guarantee zero-terminated string.
    bpf verifier checks that output buffer is zero-initialized,
    so even for short task names the output buffer don't have junk bytes.
    Note it's not a security concern, since kprobe+bpf is root only.
    
    Fixes: ffeedafbf023 ("bpf: introduce current->pid, tgid, uid, gid, comm accessors")
    Reported-by: Tobias Waldekranz <tobias@waldekranz.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/bpf/helpers.c
+++ kernel/bpf/helpers.c
@@ -149,7 +149,7 @@ BPF_CALL_2(bpf_get_current_comm, char *, buf, u32, size)
 
 	strncpy(buf, task->comm, size);
 
-	memcpy(buf, task->comm, min_t(size_t, size, sizeof(task->comm)));
+	strlcpy(buf, task->comm, min_t(size_t, size, sizeof(task->comm)));
 	return 0;
 err_clear:
 	memset(buf, 0, size);
