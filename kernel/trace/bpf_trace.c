3b1586091f6a77fd915bf2669c0eaf69bfea0a9f
Alexei Starovoitov <ast@fb.com>
2016-07-06 22:38:36 -0700

    bpf: introduce bpf_get_current_task() helper
    over time there were multiple requests to access different data
    structures and fields of task_struct current, so finally add
    the helper to access 'current' as-is. Tracing bpf programs will do
    the rest of walking the pointers via bpf_probe_read().
    Note that current can be null and bpf program has to deal it with,
    but even dumb passing null into bpf_probe_read() is still safe.
    
    Suggested-by: Brendan Gregg <brendan.d.gregg@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -377,6 +377,17 @@ u64 bpf_event_output(struct bpf_map *map, u64 flags, void *meta, u64 meta_size,
 	return __bpf_perf_event_output(regs, map, flags, &raw);
 }
 
+static u64 bpf_get_current_task(u64 r1, u64 r2, u64 r3, u64 r4, u64 r5)
+{
+	return (long) current;
+}
+
+static const struct bpf_func_proto bpf_get_current_task_proto = {
+	.func		= bpf_get_current_task,
+	.gpl_only	= true,
+	.ret_type	= RET_INTEGER,
+};
+
 static const struct bpf_func_proto *tracing_func_proto(enum bpf_func_id func_id)
 {
 	switch (func_id) {
