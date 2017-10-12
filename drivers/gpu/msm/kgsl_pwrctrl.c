5999d5b89cc89c67d7a522a12ebc115499262ab9
Kyle Piefer <kpiefer@codeaurora.org>
2017-10-12 16:20:36 -0700

    Revert "msm: kgsl: Disallow L2PC during wake up from SLUMBER"
    This reverts commit 5344e5c78f5820dfa34cfcea4572d8e347a018ce.
    The change negatively impacted performance.
    
    CRs-Fixed: 2120475
    Change-Id: Ib6ff329a3501e77d990c2e9502ed35e041f730c8
    Signed-off-by: Kyle Piefer <kpiefer@codeaurora.org>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -43,6 +43,13 @@
 
 #define DEFAULT_BUS_P 25
 
+/*
+ * The effective duration of qos request in usecs. After
+ * timeout, qos request is cancelled automatically.
+ * Kept 80ms default, inline with default GPU idle time.
+ */
+#define KGSL_L2PC_CPU_TIMEOUT	(80 * 1000)
+
 /* Order deeply matters here because reasons. New entries go on the end */
 static const char * const clocks[] = {
 	"src_clk",
@@ -510,6 +517,33 @@ void kgsl_pwrctrl_set_constraint(struct kgsl_device *device,
 	}
 }
 EXPORT_SYMBOL(kgsl_pwrctrl_set_constraint);
+
+/**
+ * kgsl_pwrctrl_update_l2pc() - Update existing qos request
+ * @device: Pointer to the kgsl_device struct
+ *
+ * Updates an existing qos request to avoid L2PC on the
+ * CPUs (which are selected through dtsi) on which GPU
+ * thread is running. This would help for performance.
+ */
+void kgsl_pwrctrl_update_l2pc(struct kgsl_device *device)
+{
+	int cpu;
+
+	if (device->pwrctrl.l2pc_cpus_mask == 0)
+		return;
+
+	cpu = get_cpu();
+	put_cpu();
+
+	if ((1 << cpu) & device->pwrctrl.l2pc_cpus_mask) {
+		pm_qos_update_request_timeout(
+				&device->pwrctrl.l2pc_cpus_qos,
+				device->pwrctrl.pm_qos_cpu_mask_latency,
+				KGSL_L2PC_CPU_TIMEOUT);
+	}
+}
+EXPORT_SYMBOL(kgsl_pwrctrl_update_l2pc);
 
 static ssize_t kgsl_pwrctrl_thermal_pwrlevel_store(struct device *dev,
 					 struct device_attribute *attr,
@@ -2165,6 +2199,9 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 		goto error_cleanup_regulators;
 
 	pwr->power_flags = 0;
+
+	kgsl_property_read_u32(device, "qcom,l2pc-cpu-mask",
+			&pwr->l2pc_cpus_mask);
 
 	pm_runtime_enable(&pdev->dev);
 
