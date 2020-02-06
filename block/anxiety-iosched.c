169db5f02c47736fd47adf296e4222dbe6581054
Tyler Nijmeh <tylernij@gmail.com>
2020-02-06 15:38:06 -0800

    anxiety-iosched: Alter queue drainage strategy
    Also, abolish the anxiety_can_dispatch method.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- block/anxiety-iosched.c
+++ block/anxiety-iosched.c
@@ -28,10 +28,10 @@ struct anxiety_data {
 	uint8_t read_ratio;
 };
 
-static inline bool anxiety_can_dispatch(struct anxiety_data *adata)
+static inline struct request *anxiety_next_entry(struct list_head *queue)
 {
-	return !list_empty(&adata->queue[READ]) ||
-			!list_empty(&adata->queue[WRITE]);
+	return list_first_entry(queue, struct request,
+		queuelist);
 }
 
 static void anxiety_merged_requests(struct request_queue *q, struct request *rq,
@@ -82,21 +82,21 @@ static uint16_t anxiety_dispatch_drain(struct request_queue *q)
 	uint16_t dispatched;
 
 	/*
-	 * Fallback to non-bias request dispatching when a mandatory
-	 * queue drain has been requested.
+	 * Drain out all of the synchronous requests first,
+	 * then drain the asynchronous requests.
 	 */
-	while (anxiety_can_dispatch(adata)) {
-		if (!list_empty(&adata->queue[SYNC])) {
-			__anxiety_dispatch(q,
-					anxiety_next_entry(&adata->queue[SYNC]));
-			dispatched++;
-		}
+	while (!list_empty(&adata->queue[SYNC])) {
+		__anxiety_dispatch(q,
+			anxiety_next_entry(&adata->queue[SYNC]));
 
-		if (!list_empty(&adata->queue[ASYNC])) {
-			__anxiety_dispatch(q,
-					anxiety_next_entry(&adata->queue[ASYNC]));
-			dispatched++;
-		}
+		dispatched++;
+	}
+
+	while (!list_empty(&adata->queue[ASYNC])) {
+		__anxiety_dispatch(q,
+			anxiety_next_entry(&adata->queue[ASYNC]));
+
+		dispatched++;
 	}
 
 	return dispatched;
@@ -108,7 +108,8 @@ static int anxiety_dispatch(struct request_queue *q, int force)
 	uint8_t batched;
 
 	/* Make sure we can even process any requests at all */
-	if (!anxiety_can_dispatch(adata))
+	if (list_empty(&adata->queue[SYNC]) &&
+		list_empty(&adata->queue[ASYNC]))
 		return 0;
 
 	/*
