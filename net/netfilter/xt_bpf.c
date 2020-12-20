9b8336e2aca9c21c00c7e54001b863fd34ba268f
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-12-20 08:50:46 +0700

    Merge remote-tracking branch 'android-4.4-p' into Mystic-4.4
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    
    Conflicts:
    	arch/arm64/boot/Makefile
    	arch/arm64/include/asm/mmu_context.h
    	arch/arm64/kernel/head.S
    	arch/mips/math-emu/cp1emu.c
    	arch/x86/configs/x86_64_cuttlefish_defconfig
    	drivers/android/binder.c
    	drivers/block/loop.c
    	drivers/hid/Makefile
    	fs/ext4/xattr.c
    	fs/f2fs/dir.c
    	fs/f2fs/namei.c
    	include/linux/thread_info.h
    	include/net/ip6_fib.h
    	net/core/netpoll.c
    	net/ipv6/route.c
    	net/netfilter/xt_quota2.c
    	security/selinux/ss/services.c

--- net/netfilter/xt_bpf.c
+++ net/netfilter/xt_bpf.c
@@ -125,6 +125,7 @@ static struct xt_match bpf_mt_reg[] __read_mostly = {
 		.match		= bpf_mt,
 		.destroy	= bpf_mt_destroy,
 		.matchsize	= sizeof(struct xt_bpf_info),
+		.usersize	= offsetof(struct xt_bpf_info, filter),
 		.me		= THIS_MODULE,
 	},
 	{
