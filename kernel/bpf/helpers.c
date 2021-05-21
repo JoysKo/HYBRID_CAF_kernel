532b8c470401ff752190b32d8fceddd15b35ef39
Anay Wadhera <awadhera@berkeley.edu>
2021-05-20 22:00:00 -0700

    Revert "bpf: avoid copying junk bytes in bpf_get_current_comm()"
    This reverts commit e8e43232627082328fa4016fab1960360360f167.
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/bpf/helpers.c
+++ kernel/bpf/helpers.c
@@ -149,11 +149,7 @@ BPF_CALL_2(bpf_get_current_comm, char *, buf, u32, size)
 
 	strncpy(buf, task->comm, size);
 
-	/* Verifier guarantees that size > 0. For task->comm exceeding
-	 * size, guarantee that buf is %NUL-terminated. Unconditionally
-	 * done here to save the size test.
-	 */
-	buf[size - 1] = 0;
+	memcpy(buf, task->comm, min_t(size_t, size, sizeof(task->comm)));
 	return 0;
 err_clear:
 	memset(buf, 0, size);
