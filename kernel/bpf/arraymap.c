c111468c4546ffdf3cebe52e4f7224d3acba0e10
Anay Wadhera <awadhera@berkeley.edu>
2021-05-20 21:56:42 -0700

    Revert "bpf, array: fix overflow in max_entries and undefined behavior in index_mask"
    This reverts commit 095b0ba360ff9a86c592c1293602d42a9297e047.
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- kernel/bpf/arraymap.c
+++ kernel/bpf/arraymap.c
@@ -56,7 +56,6 @@ static struct bpf_map *array_map_alloc(union bpf_attr *attr)
 	bool unpriv = !capable(CAP_SYS_ADMIN);
 	u64 cost, array_size, mask64;
 	struct bpf_array *array;
-	int ret;
 
 	/* check sanity of attributes */
 	if (attr->max_entries == 0 || attr->key_size != 4 ||
@@ -73,25 +72,13 @@ static struct bpf_map *array_map_alloc(union bpf_attr *attr)
 	elem_size = round_up(attr->value_size, 8);
 
 	max_entries = attr->max_entries;
+	index_mask = roundup_pow_of_two(max_entries) - 1;
 
-	/* On 32 bit archs roundup_pow_of_two() with max_entries that has
-	 * upper most bit set in u32 space is undefined behavior due to
-	 * resulting 1U << 32, so do it manually here in u64 space.
-	 */
-	mask64 = fls_long(max_entries - 1);
-	mask64 = 1ULL << mask64;
-	mask64 -= 1;
-
-	index_mask = mask64;
-	if (unpriv) {
+	if (unpriv)
 		/* round up array size to nearest power of 2,
 		 * since cpu will speculate within index_mask limits
 		 */
 		max_entries = index_mask + 1;
-		/* Check for overflows. */
-		if (max_entries < attr->max_entries)
-			return ERR_PTR(-E2BIG);
-	}
 
 	array_size = sizeof(*array);
 	if (percpu)
