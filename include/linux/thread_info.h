e87e09922853443d5189347d75b108b722cb79b0
JoysKo <joys.ko75@gmail.com>
2023-05-03 23:50:48 +0300

    sched/core: Allow putting thread_info into task_struct

--- include/linux/thread_info.h
+++ include/linux/thread_info.h
@@ -11,16 +11,15 @@
 #include <linux/bug.h>
 #include <linux/restart_block.h>
 
-struct timespec;
-struct compat_timespec;
-
 #ifdef CONFIG_THREAD_INFO_IN_TASK
+/*
+ * For CONFIG_THREAD_INFO_IN_TASK kernels we need <asm/current.h> for the
+ * definition of current, but for !CONFIG_THREAD_INFO_IN_TASK kernels,
+ * including <asm/current.h> can cause a circular dependency on some platforms.
+ */
+#include <asm/current.h>
 #define current_thread_info() ((struct thread_info *)current)
 #endif
-
-/*
- * System call restart block.
- */
 
 #include <linux/bitops.h>
 #include <asm/thread_info.h>
