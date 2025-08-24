7363897ef3da29c63f99e2060ef7fb7002220d54
JoysKo <joys.ko75@gmail.com>
2025-08-24 21:35:08 +0300

    pci: sps: Merge from Mystic kernel

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -6645,9 +6645,7 @@ static struct platform_driver msm_pcie_driver = {
 int __init pcie_init(void)
 {
 	int ret = 0, i;
-#ifdef CONFIG_IPC_LOGGING
 	char rc_name[MAX_RC_NAME_LEN];
-#endif
 
 	pr_alert("pcie:%s.\n", __func__);
 
@@ -6657,7 +6655,6 @@ int __init pcie_init(void)
 	mutex_init(&enumerate_lock);
 
 	for (i = 0; i < MAX_RC_NUM; i++) {
-#ifdef CONFIG_IPC_LOGGING
 		snprintf(rc_name, MAX_RC_NAME_LEN, "pcie%d-short", i);
 #ifdef CONFIG_IPC_LOGGING
 		msm_pcie_dev[i].ipc_log =
