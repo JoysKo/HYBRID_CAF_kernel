7cba6d5df8d06883851402cd8ed9be1f03800912
JoysKo <joys.ko75@gmail.com>
2025-09-26 06:56:55 +0300

    tune: Fix compilate warnings

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -460,10 +460,10 @@ int schedtune_can_attach(struct cgroup_taskset *tset)
 		 * conditions with migration code while the task is being
 		 * accounted
 		 */
-		rq = lock_rq_of(task, &irq_flags);
+		rq = lock_rq_of(task, (struct rq_flags *)&irq_flags);
 
 		if (!task->on_rq) {
-			unlock_rq_of(rq, task, &irq_flags);
+			unlock_rq_of(rq, task, (struct rq_flags *)&irq_flags);
 			continue;
 		}
 
@@ -484,7 +484,7 @@ int schedtune_can_attach(struct cgroup_taskset *tset)
 		 */
 		if (unlikely(dst_bg == src_bg)) {
 			raw_spin_unlock(&bg->lock);
-			unlock_rq_of(rq, task, &irq_flags);
+			unlock_rq_of(rq, task, (struct rq_flags *)&irq_flags);
 			continue;
 		}
 
@@ -499,7 +499,7 @@ int schedtune_can_attach(struct cgroup_taskset *tset)
 		bg->group[dst_bg].tasks += 1;
 
 		raw_spin_unlock(&bg->lock);
-		unlock_rq_of(rq, task, &irq_flags);
+		unlock_rq_of(rq, task, (struct rq_flags *)&irq_flags);
 
 		/* Update CPU boost group */
 		if (bg->group[src_bg].tasks == 0 || bg->group[dst_bg].tasks == 1)
@@ -570,7 +570,7 @@ void schedtune_exit_task(struct task_struct *tsk)
 	if (!unlikely(schedtune_initialized))
 		return;
 
-	rq = lock_rq_of(tsk, &irq_flags);
+	rq = lock_rq_of(tsk, (struct rq_flags *)&irq_flags);
 	rcu_read_lock();
 
 	cpu = cpu_of(rq);
@@ -579,7 +579,7 @@ void schedtune_exit_task(struct task_struct *tsk)
 	schedtune_tasks_update(tsk, cpu, idx, DEQUEUE_TASK);
 
 	rcu_read_unlock();
-	unlock_rq_of(rq, tsk, &irq_flags);
+	unlock_rq_of(rq, tsk, (struct rq_flags *)&irq_flags);
 }
 
 int schedtune_cpu_boost(int cpu)
