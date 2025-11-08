4593a2cedbf8435fc41801acec82e0bee9100ad2
JoysKo <joys.ko75@gmail.com>
2025-11-08 23:45:31 +0300

    Merge branch 'for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/viro/vfs

--- fs/internal.h
+++ fs/internal.h
@@ -154,6 +154,13 @@ extern void mnt_pin_kill(struct mount *m);
  */
 extern struct dentry_operations ns_dentry_operations;
 
+/*
+ * fs/ioctl.c
+ */
+extern int do_vfs_ioctl(struct file *file, unsigned int fd, unsigned int cmd,
+		    unsigned long arg);
+extern long vfs_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
+
 #ifdef CONFIG_FILE_TABLE_DEBUG
 void global_filetable_print_warning_once(void);
 void global_filetable_add(struct file *filp);
