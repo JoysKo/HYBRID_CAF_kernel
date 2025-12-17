92e53cb4453c429be5a2267919eb0a9367518db9
JoysKo <joys.ko75@gmail.com>
2025-12-17 20:08:34 +0300

    Merge branch 'x86-urgent-for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/tip/tip

--- arch/x86/lib/delay.c
+++ arch/x86/lib/delay.c
@@ -109,7 +109,7 @@ static void delay_mwaitx(unsigned long __loops)
 		 * Use cpu_tss as a cacheline-aligned, seldomly
 		 * accessed per-cpu variable as the monitor target.
 		 */
-		__monitorx(this_cpu_ptr(&cpu_tss), 0, 0);
+		__monitorx(raw_cpu_ptr(&cpu_tss), 0, 0);
 
 		/*
 		 * AMD, like Intel's MWAIT version, supports the EAX hint and
