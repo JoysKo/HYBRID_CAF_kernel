6896f6cd3fb9054fe193ca7bca8b358d201e9bef
JoysKo <joys.ko75@gmail.com>
2026-01-06 08:58:49 +0300

    ksu: Disabled KernelSU-Next

--- fs/exec.c
+++ fs/exec.c
@@ -1537,11 +1537,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
-#endif
-
 /*
  * sys_execve() executes a new program.
  */
@@ -1555,9 +1550,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-#ifdef CONFIG_KSU
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
+
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
